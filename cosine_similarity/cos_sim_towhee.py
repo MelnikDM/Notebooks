c4ef359fbfb50926332904fb2bb57aa8e4a60149
MelnikDM <63dmsk@gmail.com>
2024-02-19 17:11:48 +0300

    Towhee

--- cosine_similarity/cos_sim_towhee.py
+++ cosine_similarity/cos_sim_towhee.py
@@ -10,6 +10,8 @@ from keras.applications import MobileNetV2
 from keras.applications.mobilenet import preprocess_input
 from sklearn.metrics.pairwise import cosine_similarity
 from towhee import AutoPipes
+from PIL import Image
+from random import randint
 
 import cv2
 import matplotlib.pyplot as plt
@@ -85,40 +87,58 @@ def collect_all_images(dir_test):
 
 
 def flatten_pixels(img_path):
-    # Load the image onto python
+    """
+    Данная функция отвечает за препроцессинг входных данных с помощью бибилиотеки towhee.
+    Используя towhee мы преобразуем наши изображения в вектора (эмбеддинги)
+    
+    :param img_path: путь до изображений.
+    
+    Returns:
+        image_df: Датафрейм со столбцами: img_path, embedding_tt.
+    
+    """
     image_df = pd.DataFrame(img_path, columns=['img_path'])
     print(image_df.shape)
-
     p = AutoPipes.pipeline('image-embedding')
     image_df['embedding_tt'] = image_df['img_path'].apply(lambda x: np.squeeze(p(x).get()))
 
     return image_df
 
 
-def plot_similar(df, embedding_col, query_index, k_neighbors=3):
-    '''Helper function to take a dataframe index as input query and display the k nearest neighbors
-    '''
+def plot_similar(df, embedding_col, dir_out, query_index, k_neighbors=3):
+    """
+    Данная функция отвечает за:
+     - подсчет косинусного сходства;
+     - определяет количество отображаемых ближайших соседей;
+     - сохраняет результаты в .csv и в .png.
+    
+    :param df: путь до изображений;
+    :param embedding_col: целевой столбец с эмбеддингами;
+    :param dir_out: путь до output;
+    :param query_index: целевое изображение;
+    :param k_neighbors: количество соседей.
+            
+    """
 
-    # Calculate pairwise cosine similarities between query and all rows
+    # Считаем косинусное сходство и сохраняем все в .csv
     cos_similarity = cosine_similarity([df[embedding_col][query_index]], df[embedding_col].values.tolist())[0]
     cosine_dataframe = pd.DataFrame(cos_similarity)
-    cosine_dataframe.to_csv(f'{DIR_OUTPUT}/cosine_dataframe.csv', sep='\t')
+    cosine_dataframe.to_csv(f'{dir_out}/cos_df.csv', sep=';')
     print()
-    print(f"Результаты сохранены в {DIR_OUTPUT}")
+    print(f"Результаты сохранены в {dir_out}")
     print()
 
-
-    # Find nearest neighbor indices
+    # Находим ближайших соседей
     k = k_neighbors+1
     img_list = np.argsort(-cos_similarity)[:k]
     img_list = img_list[img_list != query_index]
 
-    # Plot input image
+    # Отображение изображения из запроса
     img = Image.open(df['img_path'][query_index]).convert('RGB')
     plt.imshow(img)
     plt.title('Изображение из запроса')
 
-    # Plot nearest neighbors images
+    # Отображение ближайших соседей
     fig = plt.figure(figsize=(20,4))
     plt.suptitle('Похожие изображения')
     for i, image in enumerate(img_list):
@@ -126,6 +146,9 @@ def plot_similar(df, embedding_col, query_index, k_neighbors=3):
         img = Image.open(df['img_path'][image]).convert('RGB')
         plt.imshow(img)
         plt.title(f'Cosine similiarity: {cos_similarity[image]:.3f}')
+    plt.savefig(f"{dir_out}/output.png")
+    plt.show()
+    plt.close()
     plt.tight_layout()
 
 
@@ -151,19 +174,25 @@ def main(args):
 
     # Определение пути до OUTPUT
     if args['output'] == None:
-        DIR_OUTPUT = data_configs['DIR_OUTPUT']
-        if not os.path.exists(DIR_OUTPUT):
-            os.makedirs(DIR_OUTPUT)
+       DIR_OUTPUT = data_configs['DIR_OUTPUT']
+       if not os.path.exists(DIR_OUTPUT):
+            os.makedirs(DIR_OUTPUT, exist_ok=True)
     else:
-        DIR_OUTPUT = args['output']
+         DIR_OUTPUT = args['output']
 
     # Препроцессинг изображений
     image_df = flatten_pixels(input_images)
 
+    a = int(input("Введите номер изображения: "))
+    print()
+    b = int(input("Введите количество соседей : "))
+    print()
+
     plot_similar(df=image_df,
                 embedding_col='embedding_tt',
-                query_index=randint(0, len(image_df)), # Query a random image
-                k_neighbors=3)
+                dir_out = DIR_OUTPUT,
+                query_index=a,
+                k_neighbors=b)
 
 
 if __name__ == '__main__':
